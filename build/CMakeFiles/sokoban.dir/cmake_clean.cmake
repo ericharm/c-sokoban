f77a1634238cbdd67e4928792655741e179f3e0c
Eric Harm <ericharm@gmail.com>
2022-11-20 13:11:50 -0500

    cmake

---
+++ build/CMakeFiles/sokoban.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sokoban.dir/main.c.o"
+  "CMakeFiles/sokoban.dir/main.c.o.d"
+  "sokoban"
+  "sokoban.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sokoban.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/src.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ericharm/Documents/hello-c/src/app.c" "src/CMakeFiles/src.dir/app.c.o" "gcc" "src/CMakeFiles/src.dir/app.c.o.d"
+  "/Users/ericharm/Documents/hello-c/src/boulder.c" "src/CMakeFiles/src.dir/boulder.c.o" "gcc" "src/CMakeFiles/src.dir/boulder.c.o.d"
+  "/Users/ericharm/Documents/hello-c/src/colors.c" "src/CMakeFiles/src.dir/colors.c.o" "gcc" "src/CMakeFiles/src.dir/colors.c.o.d"
+  "/Users/ericharm/Documents/hello-c/src/entity.c" "src/CMakeFiles/src.dir/entity.c.o" "gcc" "src/CMakeFiles/src.dir/entity.c.o.d"
+  "/Users/ericharm/Documents/hello-c/src/game.c" "src/CMakeFiles/src.dir/game.c.o" "gcc" "src/CMakeFiles/src.dir/game.c.o.d"
+  "/Users/ericharm/Documents/hello-c/src/player.c" "src/CMakeFiles/src.dir/player.c.o" "gcc" "src/CMakeFiles/src.dir/player.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
